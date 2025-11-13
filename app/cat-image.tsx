fbaae76039b723a54e051491d782cf55f94f7907
zyu25001-png <z_yu25001@zenrin-datacom.net>
2025-11-13 13:04:20 +0900

    Fix CSS import and update .env for CAT_API_KEY

--- app/cat-image.tsx
+++ app/cat-image.tsx
@@ -1,26 +1,21 @@
 "use client";
-
 import { useState } from "react";
 import { fetchImage } from "./fetch-image";
-import styles from "./page.module.css"; // 追加
-
+import styles from "./page.module.css";
 type CatImageProps = {
   url: string;
 };
-
 export function CatImage({ url }: CatImageProps) {
   const [imageUrl, setImageUrl] = useState<string>(url);
-
   const refreshImage = async () => {
-    setImageUrl(""); // 初期化
+    setImageUrl("");
     const image = await fetchImage();
     setImageUrl(image.url);
   };
-
   return (
     <div className={styles.page}>
       <button onClick={refreshImage} className={styles.button}>
-        他のにゃんこも見る
+        One more cat!
       </button>
       <div className={styles.frame}>
         {imageUrl && <img src={imageUrl} className={styles.img} />}
